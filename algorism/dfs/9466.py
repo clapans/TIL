c08b6cf2416005cac2bff15a18adcd7fb62481a2
clapans <clapans555@gmail.com>
2022-02-01 23:31:39 +0900

    0201

--- algorism/dfs/9466.py
+++ algorism/dfs/9466.py
@@ -3,20 +3,25 @@ import sys
 sys.setrecursionlimit(10**6)
 for case in range(int(sys.stdin.readline())):
     student = int(sys.stdin.readline())
-    students = [0] + [0 for t in range(student)]
+    students = [0] + [1 for t in range(student)]
     collect = [0] + list(map(int,sys.stdin.readline().split()))
+    res = set()
     def dfs(x):
         tmp = collect[x]
-        if students[tmp] == 0:
-            pass
+        if students[tmp] == 1:
+            students[tmp] = 0
+            v.append(tmp)
+            dfs(tmp)
         else:
-            if students[tmp] == 0:
-                v.append(tmp)
-                dfs(v)
-            else:
+            try:
                 for t in v[v.index(tmp):]:
-                    students[t] = 0
+                    res.add(t)
+            except:
+                pass
+
     for i in range(1,student+1):
         if students[i] == 1:
-            dfs([i])
-    print(sum(students))
+            v = [i]
+            students[i] = 0
+            dfs(i)
+    print(student-len(res))
