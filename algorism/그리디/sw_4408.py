7e73b108eec40ccebc8e78e6068d8d6a8949effd
clapans <clapans555@gmail.com>
2022-02-18 20:28:01 +0900

    0218

--- algorism/그리디/sw_4408.py
+++ algorism/그리디/sw_4408.py
@@ -1,22 +1,21 @@
+import heapq
+
 for case in range(int(input())):
     n = int(input())
-    greed = []
     arr = []
     for t in range(n):
         arr.append(sorted(list(map(int,input().split()))))
     arr.sort(key = lambda x : x[0])
-    for t in arr:
+    greed = []
+    heapq.heappush(greed,arr[0][1])
+
+    for t in arr[1:]:
         s = t[0] if t[0] % 2 else t[0] - 1
         e = t[1]+1 if t[1] % 2 else t[1]
-        for i in range(len(greed)):
-            for d in range(s,e+1):
-                if d in greed[i]:
-                    break
-            else:
-                ch = 0
-                greed[i] += [_ for _ in range(s,e+1)]
-                break
+        if s < greed[0]:
+            heapq.heappush(greed,e)
         else:
-            greed.append([_ for _ in range(s,e+1)])
-    print(greed)
+            heapq.heappop(greed)
+            heapq.heappush(greed,e)
+
     print(f'#{case+1} {len(greed)}')
