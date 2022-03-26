3b1854e770d2acdda0aafdceca84e05b3c984fef
clapans <clapans555@gmail.com>
2022-03-26 20:50:16 +0900

    0326

--- java/14499/14499/src/Main.java
+++ java/14499/14499/src/Main.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main{
-  int[] dx = {1,-1,0,0};
-  int[] dy = {0,0,1,-1};
+  static int[] dx = {0,0,0,-1,1};
+  static int[] dy = {0,1,-1,0,0};
+  static int[] dice = {0,0,0,0,0,0};
+  static int[][] arr = {{-1,5,4,1,3},{-1,5,4,2,0},{-1,5,4,3,1},
+                        {-1,5,4,0,2},{-1,};
+
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -12,14 +17,28 @@ public class Main{
     int x = sc.nextInt();
     int y = sc.nextInt();
     int k = sc.nextInt();
-    sc.nextLine();
-    ArrayList<Integer[]> map = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> map = new ArrayList<>();
     for (int i = 0;i < n; i++){
       ArrayList<Integer> tmp = new ArrayList<>();
-      for (int j = 0;j < 2; j++){
+      for (int j = 0;j < m; j++){
         tmp.add(sc.nextInt());
       }
       map.add(tmp);
     }
+    ArrayList<Integer> command = new ArrayList<>();
+    for (int i = 0;i < k; i++){
+      command.add(sc.nextInt());
+    }
+    for (int i:command){
+      if (0 < i & i < 5){
+        x += dx[i];
+        y += dy[i];
+        
+      }
+      
+    }
+    HashMap<Integer,Integer> maps = new HashMap<>();
+    maps.put(0,1);
+
   }
 }
