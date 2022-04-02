4790f1437661cb7119a8d16d7165f0ef1070b023
clapans <clapans555@gmail.com>
2022-04-02 22:51:22 +0900

    0402

--- java/16947/16947/src/Main.java
+++ java/16947/16947/src/Main.java
@@ -1,17 +1,43 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main{
   static int n;
-  static ArrayList<ArrayList<Integer>> arr;
-
+  static ArrayList<Integer>[] arr;
+  static Queue<Integer> queue = new LinkedList<>();
+  static boolean[] visit;
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     n = sc.nextInt();
-    sc.nextLine();
+    arr = new ArrayList[n+1];
+    visit = new boolean[n+1];
     for (int i = 0; i < n; i++) {
-      String[] tmp = sc.nextLine().split(" ");
+      arr[i] = new ArrayList<>();
+    }
 
+    for (int i = 0; i < n; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      arr[a].add(b);
+      arr[b].add(a);
     }  
+  }
+
+  static void bfs(){
+    queue.offer(1);
+    while (queue.size() >= 1){
+      int x = queue.poll();
+      for (int i:arr[x]){
+        if (!visit[i]){
+          visit[i] = true;
+          queue.offer(i);
+        }
+        else {
+          
+        }
+      }
+    }
   }
 }
