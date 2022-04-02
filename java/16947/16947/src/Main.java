901b3e02c9e067c6e8bbfb5e0b0c77a1db90dbe4
clapans <clapans555@gmail.com>
2022-04-02 22:21:35 +0900

    0402

---
+++ java/16947/16947/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+  static int n;
+  static ArrayList<ArrayList<Integer>> arr;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    sc.nextLine();
+    for (int i = 0; i < n; i++) {
+      String[] tmp = sc.nextLine().split(" ");
+
+    }  
+  }
+}
