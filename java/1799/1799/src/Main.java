b1ac579102f212e62f27a4c228b3128f5368d521
clapans <clapans555@gmail.com>
2022-04-05 00:02:51 +0900

    0405

--- java/1799/1799/src/Main.java
+++ java/1799/1799/src/Main.java
@@ -13,28 +13,47 @@ public class Main{
   static int n;
   static int[] dx = {-1,-1};
   static int[] dy = {1,-1};
+  static int tmp_res = 0;
   static int res = 0;
+  static int[][] arr;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     n = sc.nextInt();
-    int[][] arr = new int[n][n];
-    ArrayList<Bishop> able = new ArrayList<>();
+    arr = new int[n][n];
+    ArrayList<Bishop> black = new ArrayList<>();
+    ArrayList<Bishop> white = new ArrayList<>();
+    boolean isBlack = true;
     for (int i = 0; i < n; i++){
       for (int j = 0; j < n; j++){
         int tmp = sc.nextInt();
         arr[i][j] = tmp;
         if (tmp == 1){
-          able.add(new Bishop(i, j));
+          if (isBlack){
+            black.add(new Bishop(i, j));
+          }
+          else{
+            white.add(new Bishop(i, j));
+          }
+        }
+        if (isBlack){
+          isBlack = false;
+        }
+        else{
+          isBlack = true;
         }
       }
     }
-    bishop(0, arr, 0, able);
+    bishop(0, 0, black);
+    res += tmp_res;
+    tmp_res = 0;
+    bishop(0, 0, white);
+    res += tmp_res;
     System.out.println(res);
     sc.close();
   }
 
-  static boolean check(int x, int y, int[][] arr){
+  static boolean check(int x, int y){
     for (int i = 0; i < 2; i++){
       int cnt = 1;
       while(true){
@@ -54,19 +73,18 @@ public class Main{
     return true;
   }
 
-  static void bishop(int t, int[][] arr, int cnt, ArrayList<Bishop> able){
+  static void bishop(int t, int cnt, ArrayList<Bishop> able){
     if (t == able.size()){
-      res = Math.max(res,cnt);
+      tmp_res = Math.max(tmp_res,cnt);
     }
     else{
       Bishop pos = able.get(t);
-      if (check(pos.x,pos.y,arr)){
+      if (check(pos.x,pos.y)){
         arr[pos.x][pos.y] = 2;
-        bishop(t+1, arr, cnt+1, able);
+        bishop(t+1, cnt+1, able);
         arr[pos.x][pos.y] = 1;
-      }else{
-        bishop(t+1, arr, cnt, able);
       }
+      bishop(t+1, cnt, able);
     }
   }
 }
