ac2065c670421c0e4d15017a27c8ae21cf819ac7
clapans <clapans555@gmail.com>
2022-04-07 16:48:50 +0900

    0407

--- java/16234/16234/src/Main.java
+++ java/16234/16234/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -17,6 +19,7 @@ public class Main{
   static Queue<Node> queue = new LinkedList<>();
   static int[] dx = {1,-1,0,0};
   static int[] dy = {0,0,1,-1};
+  static int check;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -33,31 +36,57 @@ public class Main{
       }
     }
 
-    for (int i = 0;i < n;i++){
-      for (int j = 0; j< n;j++){
-        if (!visit[i][j]){
-          visit[i][j] = true;
-          bfs(i,j);
+    int cnt = 0;
+
+    while (true){
+      check = 0;
+      for (boolean[] i : visit){
+        Arrays.fill(i,false);
+      }
+      for (int i = 0;i < n;i++){
+        for (int j = 0; j< n;j++){
+          if (!visit[i][j]){
+            visit[i][j] = true;
+            bfs(i,j);
+          }
         }
       }
+      if (check == 0){
+        break;
+      }
+      else{
+        cnt += 1;
+      }
     }
+    System.out.println(cnt);
     sc.close();
   }
 
   static void bfs(int x,int y){
     queue.offer(new Node(x, y));
+    ArrayList<Node> union = new ArrayList<>();
+    union.add(new Node(x,y));
+    int sum = arr[x][y];
     while (queue.size() >= 1){
       Node tmp = queue.poll();
       for (int i = 0;i < 4;i++){
         int nx = tmp.x + dx[i];
         int ny = tmp.y + dy[i];
-        if (0 <= nx && nx < n && 0 <= ny && ny < n && !visit[ny][ny]){
-          int distance = Math.abs(arr[x][y] - arr[nx][ny])
+        if (0 <= nx && nx < n && 0 <= ny && ny < n && !visit[nx][ny]){
+          int distance = Math.abs(arr[tmp.x][tmp.y] - arr[nx][ny]);
           if (l <= distance && distance <= r){
-            
+            check = 1;
+            visit[nx][ny] = true;
+            queue.add(new Node(nx,ny));
+            union.add(new Node(nx,ny));
+            sum += arr[nx][ny];
           }
         }
       }
+    }
+    int avg = sum/union.size();
+    for (Node i : union){
+      arr[i.x][i.y] = avg;
     }
   }
 }
