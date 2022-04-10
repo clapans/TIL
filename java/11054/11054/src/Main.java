60ff6cb572dc28ee27829e2707eff09ccea8473e
clapans <clapans555@gmail.com>
2022-04-10 15:26:53 +0900

    0410

--- java/11054/11054/src/Main.java
+++ java/11054/11054/src/Main.java
@@ -6,19 +6,33 @@ public class Main{
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int[] nums = new int[n];
-    int[] dp = new int[n];
+    int[][] dp = new int[2][n];
+    int res = 0;
     for (int i = 0; i < n; i++){
       nums[i] = sc.nextInt();
     }
-    Arrays.fill(dp, 1);
+    for (int i = 0; i < 2; i++){
+      Arrays.fill(dp[i],1);
+    }
     for (int i = 0; i < n; i++){
       for (int j = 0; j < i; j++){
         if (nums[j] > nums[i]){
-          dp[i] = Math.max(dp[i],dp[j]+1);
+          dp[1][i] = Math.max(dp[1][i],dp[1][j]+1);
+          dp[1][i] = Math.max(dp[1][i],dp[0][j]+1);
+        }
+        else if (nums[j] < nums[i]){
+          dp[0][i] = Math.max(dp[0][i],dp[0][j]+1);
         }
       }
     }
-
+    for (int[] t : dp){
+      for (int i : t){
+        if (res < i){
+          res = i;
+        }
+      }
+    }
+    System.out.println(res);
     sc.close();
   }
 }
