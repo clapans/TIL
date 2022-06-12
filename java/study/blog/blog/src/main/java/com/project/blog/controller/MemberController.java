def3afa19e8f6a884f85dfa4197e9283fd042fd6
clapans <clapans555@gmail.com>
2022-06-12 23:24:04 +0900

    0612

--- java/study/blog/blog/src/main/java/com/project/blog/controller/MemberController.java
+++ java/study/blog/blog/src/main/java/com/project/blog/controller/MemberController.java
@@ -4,11 +4,14 @@ import com.project.blog.domain.Member;
 import com.project.blog.domain.Role;
 import com.project.blog.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController
@@ -38,6 +41,29 @@ public class MemberController {
                 return new IllegalArgumentException("해당 유저가 없습니다. id : " + id);
             }
         });
+        return member;
+    }
+
+    @GetMapping("/dummy/user")
+    public List<Member> list(){
+        return memberRepository.findAll();
+    }
+    @GetMapping("/dummy/user/page")
+    public List<Member> pageList(@PageableDefault(size = 1,sort = "id",direction = Sort.Direction.DESC) Pageable pageable){
+        return memberRepository.findAll(pageable).getContent();
+    }
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public Member update(@PathVariable int id, @RequestBody Member member){
+        System.out.println(member.getUsername());
+        System.out.println(member.getPassword());
+        System.out.println(member.getEmail());
+        Member premember = memberRepository.findById(id).orElseThrow(() -> {
+           return new IllegalArgumentException("유저를 찾을 수 없습니다.");
+        });
+        premember.setUsername(member.getUsername());
+        premember.setPassword(member.getPassword());
         return member;
     }
 }
