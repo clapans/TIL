b82f47089911f6a5c204186b53f243ae0df190c2
clapans <clapans555@gmail.com>
2022-11-28 01:28:16 +0900

    1128

---
+++ myweb/myweb/src/main/java/com/example/myweb/controller/RouteController.java
@@ -0,0 +1,12 @@
+package com.example.myweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RouteController {
+    @GetMapping("")
+    public String getMain() {
+        return "index";
+    }
+}
